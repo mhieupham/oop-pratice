7208f79fa7f2a20dbde93a75af1ab166d1b16bbe
mhieupham1 <mhieupham1@gmail.com>
2020-04-14 11:05:58 +0700

    demo

---
+++ includes/NewClass.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+class NewClass
+{
+    public function Say(){
+        return 'a';
+    }
+}
+
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+require 'includes/NewClass.inc.php';
+$obj = new NewClass();
+var_dump($obj);
+?>
+<html>
+<head>
+
+</head>
+<body>
+
+</body>
+</html>
